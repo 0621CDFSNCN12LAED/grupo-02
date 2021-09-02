6c56d05f695d1ab77b0ceb2e3ae2681c8fcd4543
Kili90 <guido.h.ruiz@gmail.com>
2021-09-02 20:20:53 -0300

    Update Sprint 3 Partials 2 sep

--- src/routes/main-routes.js
+++ src/routes/main-routes.js
@@ -2,11 +2,9 @@ const express = require("express");
 const router = express.Router();
 const mainController = require("../controllers/main-controller");
 
-const productController = require("../controllers/product-controller");
-
 router.get("/", mainController.index);
-router.get("/Carrito", mainController.carrito);
-router.get("/Producto", mainController.producto);
+router.get("/Carrito/:id", mainController.carrito);
+router.get("/Producto/:id", mainController.producto);
 router.get("/Login", mainController.login);
 router.get("/Registro", mainController.register);
 
